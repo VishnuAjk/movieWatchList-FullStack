2ebbcfa9b38a213086112084a497c48b00400802
VISHNU <145429961+VishnuAjk@users.noreply.github.com>
2024-06-27 09:35:21 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,12 +8,15 @@ import movieRouter from "./src/Routes/movie.routes.js";
 import { connectToDatabase } from "./src/Config/mongoose.js";
 import jwtAuth from "./src/Middlewares/jwtAuth.middleware.js";
 const app = express();
-const corsOptions = {
-    origin: 'https://movie-watch-list-client.vercel.app', 
-    optionsSuccessStatus: 200
-};
+
+app.use(cors({
+    origin: ["https://movie-watch-list-client.vercel.app"],
+    methods:["POST", "GET", "DELETE"],
+    credentials:true
+}));
+
 app.use(express.json());
-app.use(cors(corsOptions));
+
 
 app.use('/user', userRouter);
 app.use('/movies',jwtAuth ,movieRouter);
