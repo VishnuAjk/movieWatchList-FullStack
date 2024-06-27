d1b8791a486a50c8989792312b5ee604ace41172
VISHNU <145429961+VishnuAjk@users.noreply.github.com>
2024-06-27 09:12:21 +0530

    Update movieSlice.js

--- Frontend/src/Redux/Reducers/movieSlice.js
+++ Frontend/src/Redux/Reducers/movieSlice.js
@@ -13,14 +13,14 @@ const config = {
 // Get movies from DB
 export const getMovies = createAsyncThunk('movies/getMovies', async (_, { getState }) => {
    
-    const response = await axios.get('http://localhost:3100/movies/get', config);
+    const response = await axios.get('https://movie-watch-list-server.vercel.app/movies/get', config);
     return response.data.movies;
   });
   
 //   Add a new movie to the watchList
   export const addMovie = createAsyncThunk('movies/add', async (movieData) => {
 
-    const response = await axios.post('http://localhost:3100/movies/add', movieData, config); 
+    const response = await axios.post('https://movie-watch-list-server.vercel.app/movies/add', movieData, config); 
     
     return response.data;
   });
@@ -29,14 +29,14 @@ export const getMovies = createAsyncThunk('movies/getMovies', async (_, { getSta
 
 export const deleteMovie = createAsyncThunk('/movies/delete', async(movieId)=>{
 
-    const response = await axios.delete(`http://localhost:3100/movies/delete/${movieId}`, config);
+    const response = await axios.delete(`https://movie-watch-list-server.vercel.app/movies/delete/${movieId}`, config);
     return movieId;
 })
 
 // Updating a movie in list
 export const updateMovie = createAsyncThunk('movies/update', async ({ _id, title, description, year, genre }) => {
     try {
-      const response = await axios.put(`http://localhost:3100/movies/update/${_id}`, { title, description, year, genre }, config);
+      const response = await axios.put(`https://movie-watch-list-server.vercel.app/movies/update/${_id}`, { title, description, year, genre }, config);
       return response.data; // Assuming your API returns the updated movie object
     } 
     catch (error) {
